d498f7ef558db37bd7fa91b32e0353e0919671da
optikrus <optikrus61@gmail.com>
2022-05-23 09:15:33 +0300

    leroy commit unresolved

--- lesson_7/productparser/items.py
+++ lesson_7/productparser/items.py
@@ -1,9 +1,9 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
+from scrapy.loader.processors import Compose, MapCompose, TakeFirst
+
+
+def get_big_img_urls(img_url: str):
+    return img_url.replace('_82', '_2000')
 
 
 class ProductparserItem(scrapy.Item):
@@ -12,6 +12,7 @@ class ProductparserItem(scrapy.Item):
     title = scrapy.Field()
     price = scrapy.Field()
     site = scrapy.Field()
-    img_urls = scrapy.Field()
+    img_urls = scrapy.Field(input_processor=MapCompose(get_big_img_urls))
+    img_info = scrapy.Field()
     article = scrapy.Field()
     params = scrapy.Field()
--- lesson_7/productparser/pipelines.py
+++ lesson_7/productparser/pipelines.py
@@ -1,13 +1,28 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+import scrapy
 from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
 
 
 class ProductparserPipeline:
     def process_item(self, item, spider):
+        # print("process item")
+        # print()
+        return item
+
+
+class ProductparserImagesPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item["img_urls"]:
+            for img_url in item["img_urls"]:
+                try:
+                    yield scrapy.Request(img_url)
+                except Exception as e:
+                    print(e)
+
+    def item_completed(self, results, item, info):
+        print("ITEM_COMPLETED")
+        if results:
+            item["img_info"] = [i[1] for i in results if i[0]]
+            del item["img_urls"]
+        print(item)
         return item
