c690f3b18e0200151d05642b8ea88534514e4c0c
optikrus <optikrus61@gmail.com>
2022-06-08 16:44:52 +0300

    wrong subscribers parse

--- lesson_8/lesson_8/vc/spiders/vcru.py
+++ lesson_8/lesson_8/vc/spiders/vcru.py
@@ -1,28 +1,62 @@
 import scrapy
-from scrapy import FormRequest
-from scrapy.http import TextResponse
+from scrapy.http import TextResponse, FormRequest
 
 
 class VcruSpider(scrapy.Spider):
     name = "vcru"
     allowed_domains = ["vc.ru"]
-    users = ["https://vc.ru/u/14066-andrey-frolov", "https://vc.ru/u/28953-alina-tolmacheva"]
-    start_urls = [i + '/details/subscribers' for i in users]
+    start_urls = ["https://vc.ru/"]
     login_url = "https://vc.ru/auth/simple/login"
-    max_page_number = 5
+    interesting_url = "https://vc.ru/u/980897-vasiliy-cherepanov"
+    subscribers = interesting_url + '/details/subscribers'
+    # max_posts_page_number = 5
+    # template_url = (
+    #     "https://vc.ru/marketing/more?"
+    #     "last_id=%s&last_sorting_value=%s"
+    #     "&page=%s&exclude_ids=[]&mode=raw"
+    # )
 
-    def parse(self, response: TextResponse, page_number: int = 1, **kwargs):
-        subscribers = response.xpath('//a[@class="v-list-subsites-item__main"]')
+    def __init__(self, login, password):
+        super().__init__()
+        self.login = login
+        self.password = password
+
+    def parse(self, response: TextResponse, **kwargs):
+        print("PARSE")
+        print(response.url)
+        version = response.xpath("//link[@rel='stylesheet'" " and contains(@href, 'vc-')]/@href").get()
+        version = version.split("/")[-1].split(".")[1]
         print()
-        for item in subscribers:
-            url = item.xpath("./@href").get()
-            yield response.follow(url, callback=self.parse_user)
+        yield FormRequest(
+            self.login_url,
+            formdata={
+                "values[login]": self.login,
+                "values[password]": self.password,
+                "mode": "raw",
+            },
+            headers={
+                "origin": response.url,
+                "referer": response.url,
+                "x-js-version": version,
+                "x-this-is-csrf": "THIS IS SPARTA!",
+            },
+            callback=self.parse_login,
+            method="POST",
+        )
 
-    def parse_user(self, response: TextResponse):
-        name_xpath = '//a[@class="v-header-title__item v-header-title__name"]/text()'
-        img_xpath = '//div[@class="v-header-avatar v-header-avatar--with-zoom"]/@style'
-
+    def parse_login(self, response: TextResponse):
+        print("PARSE_LOGIN")
         print()
+        data = response.json()
+        if data["rc"] != 200:
+            raise ValueError(f"Something went wrong with login: {data['rm']}")
+        yield response.follow(self.subscribers, callback=self.parse_subscribers)
+
+    def parse_subscribers(self, response: TextResponse):
+        print("SUBSCRIBERS")
+        subscribers = response.xpath("//a[@class='v-list-subsites-item__main']")
+        print()
+
 
 
 
